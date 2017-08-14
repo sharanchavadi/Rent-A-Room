c791e09d3da994bb091368ba7d06f045c5fbbb76
sharan <sharanschavadi@gmail.com>
2017-08-14 21:13:31 +0530

    added associations to user,role,city,aminity models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  has_many :cities
+  has_many :aminities
+  belongs_to :role
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
