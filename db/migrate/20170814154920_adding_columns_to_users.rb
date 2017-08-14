16aca3aeda87099324289aa0a13c76a7a0082a00
sharan <sharanschavadi@gmail.com>
2017-08-14 21:27:26 +0530

    added columns to user model

---
+++ db/migrate/20170814154920_adding_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddingColumnsToUsers < ActiveRecord::Migration
+  def change
+
+  	add_column :users, :first_name, :string
+  	add_column :users, :last_name, :string
+  	add_column :users, :username, :string
+  	add_column :users, :mobile, :string
+  	add_column :users, :role_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814152004) do
+ActiveRecord::Schema.define(version: 20170814154920) do
 
   create_table "amenities", force: :cascade do |t|
     t.string   "name"
@@ -107,6 +107,11 @@ ActiveRecord::Schema.define(version: 20170814152004) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "mobile"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
