5d16c0e96d6675c894c99cb1a3b231246ef7d6db
sharan <sharanschavadi@gmail.com>
2017-08-14 20:55:09 +0530

    User management setup using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :amenities
   resources :cities
   resources :roles
@@ -6,7 +7,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+  root 'cities#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
