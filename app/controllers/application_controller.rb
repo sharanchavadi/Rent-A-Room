6f32169ac524d72ba3e64903f1ffa285ed8d31b6
sharan <sharanschavadi@gmail.com>
2017-08-16 14:17:51 +0530

    overriding devise conventions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   	redirect_to root_path, notice: "Unauthorized Access"
   end
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+         user_params.permit({ keys: [] }, :first_name, :last_name, :username, :mobile, :email, :password, :role_id, :password_confirmation)
+      end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,19 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name, :username, :email, :mobile
-  validates_numericality_of :mobile, :role_id
+  validates_numericality_of :mobile
   validates_length_of :mobile, is:10
 
   after_create :setup_user_role
 
   def role?(role)
-     self.roles.pluck(:name).include? role
+     self.role.name == role
   end 
 
   private
   def setup_user_role
-     User.create(role_id: Role.find_by(name: "user").id)
-   end
+     self.role_id = Role.find_by(name: "guest").id
+     self.save
+  end
 
 end
